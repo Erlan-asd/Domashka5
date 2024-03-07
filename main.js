46ba0d76a154bbdc07e90be2aa8b577a4d6aeada
Erlan-asd <Vipoffacc@gmail.com>
2024-03-08 00:42:34 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// 1)----------------------------------------------------
+
+// const arr1 = ['a', 'b', 'c', 'd'];
+// const result = arr1.join("-");
+// console.log(result);
+
+
+// 2)----------------------------------------------------
+
+// let inputString = prompt("Введите строку:");
+// let words = inputString.split(" ");
+// console.log("Слова введенной строки:");
+// for (let i = 0; i < words.length; i++) {
+//     console.log(words[i]);
+// }
+
+
+// 3)----------------------------------------------------
+
+// let date = "2024-03-07";
+// let result = date.split("-").reverse().join('.');
+// console.log(result);
+
+// 4)----------------------------------------------------
+
+let usersRecords = [ {name: 'John', id: 123, marks : 98, pole: "mentor", age: 35 },
+          {name: 'Baba', id: 101, marks : 23, pole: "student", age: 12 },
+          {name: 'yaga', id: 200, marks : 45 , pole: "student", age: 18},
+          {name: 'Wick', id: 115, marks : 75 , pole: "mentor", age: 24} ];
+
+
+
+// let result = usersRecords.map((item) => item.name);
+// usersRecords.forEach((item) => alert(item.name))
+// console.log(result);
+
+
+// 5)----------------------------------------------------
+
+// let findIndex = usersRecords.findIndex((item) => item.age === 18);
+// console.log(findIndex);
+
+
+// 6)----------------------------------------------------
+
+// let usersName = usersRecords.map((item) => item.name).join(" ");
+// console.log(usersName);
+
+
+// 7)----------------------------------------------------
+
+// let filtr = usersRecords.filter((item) => item.marks>44); 
+// let result = filtr.map((item) => item.marks+20 );
+// console.log(filtr);
+// console.log(result);
